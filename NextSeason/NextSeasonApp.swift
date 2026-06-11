de6ef5f647e3a340a05cfd55f5828f0d224452fc
Janine Ohmer <janine@trialbyfyre.com>
2026-06-11 16:23:59 -0700

    Initial empty project

---
+++ NextSeason/NextSeasonApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextSeasonApp.swift
+//  NextSeason
+//
+//  Created by Janine Ohmer on 6/11/26.
+//
+
+import SwiftUI
+
+@main
+struct NextSeasonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
